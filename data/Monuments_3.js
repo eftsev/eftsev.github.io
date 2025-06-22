e7d652d945e151875daef2b391fc3930174781a9
eftsev <esevvas@gmail.com>
2025-06-22 16:01:13 +0300

    Add files via upload

---
+++ data/Monuments_3.js
@@ -0,0 +1,202 @@
+var json_Monuments_3 = {
+  "type": "FeatureCollection",
+  "name": "Monuments_3",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Big Ben",
+        "Country": "England",
+        "City": "London",
+        "Flag": "0",
+        "Description": "0",
+        "Image": "0",
+        "Audio": "0",
+        "Video": "0",
+        "3D Model": "0",
+        "QR Code": "0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-0.124635799262216, 51.500786510813462]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Brandenburg Gate",
+        "Country": "Germany",
+        "City": "Berlin",
+        "Flag": "1",
+        "Description": "1",
+        "Image": "1",
+        "Audio": "1",
+        "Video": "1",
+        "3D Model": "1",
+        "QR Code": "1"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [13.377616911266422, 52.516299926913909]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Colosseum",
+        "Country": "Italy",
+        "City": "Rome",
+        "Flag": "2",
+        "Description": "2",
+        "Image": "2",
+        "Audio": "2",
+        "Video": "2",
+        "3D Model": "2",
+        "QR Code": "2"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [12.492335707051113, 41.890220690704894]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Eiffel Tower",
+        "Country": "France",
+        "City": "Paris",
+        "Flag": "3",
+        "Description": "3",
+        "Image": "3",
+        "Audio": "3",
+        "Video": "3",
+        "3D Model": "3",
+        "QR Code": "3"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [2.294481003952980, 48.858370099999994]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "The Parthenon",
+        "Country": "Greece",
+        "City": "Athens",
+        "Flag": "4",
+        "Description": "4",
+        "Image": "4",
+        "Audio": "4",
+        "Video": "4",
+        "3D Model": "4",
+        "QR Code": "4"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [23.72609972953829, 37.97153277493394]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "Name": "Leaning Tower of Pisa",
+        "Country": "Italy",
+        "City": "Pisa",
+        "Flag": "5",
+        "Description": "5",
+        "Image": "5",
+        "Audio": "5",
+        "Video": "5",
+        "3D Model": "5",
+        "QR Code": "5"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [10.3966, 43.7229]
+      }
+    },
+            {
+      "type": "Feature",
+      "properties": {
+        "Name": "White Tower of Thessaloniki",
+        "Country": "Greece",
+        "City": "Thessaloniki",
+        "Flag": "6",
+        "Description": "6",
+        "Image": "6",
+        "Audio": "6",
+        "Video": "6",
+        "3D Model": "6",
+        "QR Code": "6"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [22.9444, 40.6401]
+      }
+    },
+                {
+      "type": "Feature",
+      "properties": {
+        "Name": "Hagia Sophia",
+        "Country": "Turkey",
+        "City": "Istanbul",
+        "Flag": "7",
+        "Description": "7",
+        "Image": "7",
+        "Audio": "7",
+        "Video": "7",
+        "3D Model": "7",
+        "QR Code": "7"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [28.9802, 41.0086]
+      }
+    },
+	         {
+      "type": "Feature",
+      "properties": {
+        "Name": "Edinburgh Castle",
+        "Country": "Scotland",
+        "City": "Edinburgh",
+        "Flag": "8",
+        "Description": "8",
+        "Image": "8",
+        "Audio": "8",
+        "Video": "8",
+        "3D Model": "8",
+        "QR Code": "8"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-3.2008, 55.9486]
+      }
+    },
+	         {
+      "type": "Feature",
+      "properties": {
+        "Name": "St. Basil's Cathedral",
+        "Country": "Russia",
+        "City": "Moscow",
+        "Flag": "9",
+        "Description": "9",
+        "Image": "9",
+        "Audio": "9",
+        "Video": "9",
+        "3D Model": "9",
+        "QR Code": "9"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [37.622658, 55.752419]
+      }
+    },
+  ]
+};
